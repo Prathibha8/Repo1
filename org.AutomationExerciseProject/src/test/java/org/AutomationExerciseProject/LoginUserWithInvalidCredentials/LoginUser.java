d3ebe61303ad8d1aa4ebb9ae8a8aa61f6fc71b67
Acer <Acer@LAPTOP-8B5MMRRI>
2025-01-27 12:30:33 +0530

    Editted_1

---
+++ org.AutomationExerciseProject/src/test/java/org/AutomationExerciseProject/LoginUserWithInvalidCredentials/LoginUser.java
@@ -0,0 +1,10 @@
+package org.AutomationExerciseProject.LoginUserWithInvalidCredentials;
+
+public class LoginUser {
+	
+	public void login() {
+		
+		System.out.println("Login Sucessfull");
+	}
+
+}
